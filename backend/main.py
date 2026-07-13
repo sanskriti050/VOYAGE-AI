e6a9f74ed39858ec9c14292b13cc2997e1f44600
sanskriti050 <sanskritiagarwal352@gmail.com>
2026-07-13 16:55:32 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -13,9 +13,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://keen-daifuku-557732.netlify.app",
-        "http://localhost:5173",
-    ],
+    "http://localhost:5173",
+    "https://voyage-pnrrx6zom-sanskriti51.vercel.app",
+    ]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
